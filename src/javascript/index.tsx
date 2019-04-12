09673fef1b3493ffcd0c2fcbd5ccb453a52b7601
CreativeBuilds <gaminggonecreative@gmail.com>
2019-04-11 20:55:28 -0400

    Succesfully added TypeScript

--- src/javascript/index.tsx
+++ src/javascript/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 const mountPoint = document.getElementById('app');
 
-ReactDOM.render(<div>Test!</div>, mountPoint);
+ReactDOM.render(<div>Test x2!</div>, mountPoint);
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,40 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // context: __dirname + '\\src\\javascript',
-    entry: './src/javascript/index.js',
-    module: {
-        rules: [
-            {
-				test: /\.html$/,
-				use: {
-					loader: 'file-loader',
-					query: {
-						name: '[name].[ext]'
-					}
-				}
-            },
-            {
-				test: /\.(sa|sc|c)ss$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-            },
-            {
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env']
-						}
-					}
-				]
-			},
-          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
+  // context: __dirname + '\\src\\javascript',
+  entry: './src/javascript/index.tsx',
+  resolve: {
+    // changed from extensions: [".js", ".jsx"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]'
+          }
+        }
       },
-      output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '/src/index.ejs')
-        })
-      ]
-}
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      { test: /\.(t|j)sx?$/, use: { loader: 'awesome-typescript-loader' } },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.ejs')
+    })
+  ]
+};
