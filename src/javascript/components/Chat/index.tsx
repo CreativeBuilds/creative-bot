f1c46e2030efc816a89cedb851cf66a76626a415
CreativeBuilds <gaminggonecreative@gmail.com>
2019-04-12 16:51:29 -0400

    Added message showing up in chat

--- src/javascript/components/Chat/index.tsx
+++ src/javascript/components/Chat/index.tsx
@@ -1,31 +1,57 @@
 import * as React from 'react';
-import { useState, useContext } from 'react';
+import { useContext, Component } from 'react';
 import { ThemeContext } from '../../helpers';
 import { MdSend } from 'react-icons/md';
+
+import {Message} from './Message';
 
 const Window: any = window;
 const { ipcRenderer } = Window.require('electron');
 
 const styles: any = require('./Chat.scss');
 
-const Chat = props => {
-  const { stateTheme, setStateTheme } = useContext(ThemeContext);
-  const [messages, setMessages] = useState([])
-  let newMessage = (event, data) => {
-    setMessages(messages.concat([data.message]))
+class ChatWrap extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
   }
-  console.log("messages", messages);
-  ipcRenderer.on('newmessage', newMessage)
+
+  componentDidMount() {
+    let newMessage = (event, data) => {
+      let newArr = [...this.state.messages, data.message];
+      this.setState({ messages: newArr });
+    };
+    ipcRenderer.on('newmessage', newMessage);
+  }
+
+  render() {
+    return (
+      <ChatComponent
+        props={Object.assign({ messages: this.state.messages }, this.props)}
+      />
+    );
+  }
+}
+
+const ChatComponent = ({ props }) => {
+  const { stateTheme, setStateTheme } = useContext(ThemeContext);
+  const { messages } = props;
+
   return (
     <div style={stateTheme.menu} className={styles.Chat}>
       <div style={stateTheme.menu.title} className={styles.header}>
         CHAT
       </div>
       <div style={{}} className={styles.content}>
+        {messages.map((message, nth) => {
+          return <Message styles={styles} message={message} stateTheme={stateTheme} nth={nth}></Message>;
+        })}
         {/* This is for the actual chat messages */}
       </div>
       <div style={stateTheme.menu['title_hover']} className={styles.input}>
-      {/* TODO change maxLength to be limitless and then send messages once every 2 seconds to get around chat slowmode */}
+        {/* TODO change maxLength to be limitless and then send messages once every 2 seconds to get around chat slowmode */}
         <textarea style={stateTheme.chat.input} maxLength={140} />
         <div className={styles.send} style={stateTheme.chat.input}>
           <MdSend />
@@ -33,6 +59,10 @@ const Chat = props => {
       </div>
     </div>
   );
+};
+
+const Chat = props => {
+  return <ChatWrap props={props} />;
 };
 
 export { Chat };
--- src/javascript/helpers/Theme.tsx
+++ src/javascript/helpers/Theme.tsx
@@ -1,56 +1,65 @@
 import * as React from 'react';
 
 const theme = {
-    dark: {
-      main: {
-        backgroundColor: '#202225',
-        color: '#f0f0f0',
-        highlightColor: '#ffd300'
+  dark: {
+    main: {
+      backgroundColor: '#202225',
+      color: '#f0f0f0',
+      highlightColor: '#ffd300'
+    },
+    menu: {
+      backgroundColor: '#141517',
+      title: {
+        'border-bottom': '4px solid #202225',
+        'font-size': '30px'
       },
-      menu: {
-          backgroundColor: '#141517',
-          title: {
-            "border-bottom": '4px solid #202225',
-            "font-size":"30px"
-          },
-          "title_hover":{
-              backgroundColor: '#0e0f10'
-          }
-      },
-      chat: {
-        input: {
-          backgroundColor: "#202225",
-          color: "#f0f0f0"
-        }
+      title_hover: {
+        backgroundColor: '#0e0f10'
       }
     },
-    light: {
-      main: {
-        backgroundColor: '#ffffff',
-        color: `#202225`,
-        highlightColor: '#ffd300'
+
+    chat: {
+      input: {
+        backgroundColor: '#202225',
+        color: '#f0f0f0'
       },
-      menu: {
-          backgroundColor: `#eeeeee`,
-          color: '#202225',
-          highlightColor: '#ffd3000',
-          title: {
-            "border-bottom": '4px solid #cccccc',
-            "font-size": "30px"
-          },
-          "title_hover":{
-              backgroundColor: '#dddddd'
-          }
-      },
-      chat: {
-        input: {
-          backgroundColor: "#ffffff",
-          color: "#202225",
+      message: {
+        backgroundColor: '#202225',
+        color: '#f0f0f0',
+        alternate: {
+          backgroundColor: 'rgb(14, 15, 16)',
+          color: '#f0f0f0'
         }
       }
     }
-  };
-  
-  const ThemeContext = React.createContext({stateTheme: theme.dark});
+  },
+  light: {
+    main: {
+      backgroundColor: '#ffffff',
+      color: `#202225`,
+      highlightColor: '#ffd300'
+    },
+    menu: {
+      backgroundColor: `#eeeeee`,
+      color: '#202225',
+      highlightColor: '#ffd3000',
+      title: {
+        'border-bottom': '4px solid #cccccc',
+        'font-size': '30px'
+      },
+      title_hover: {
+        backgroundColor: '#dddddd'
+      }
+    },
+    chat: {
+      input: {
+        backgroundColor: '#ffffff',
+        color: '#202225'
+      }
+    }
+  }
+};
 
-  export {ThemeContext, theme};
+const ThemeContext = React.createContext({ stateTheme: theme.dark });
+
+export { ThemeContext, theme };
