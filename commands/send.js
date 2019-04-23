c07ef58dbfdc4439ca5ebe86b24faa5a42d5bcc1
CreativeBuilds <gaminggonecreative@gmail.com>
2019-04-23 14:30:14 -0400

    fixed getLive

--- commands/send.js
+++ commands/send.js
@@ -4,7 +4,7 @@ const config = tryRequireFromStorage('../config');
 const run = ({ message, args }) => {
   // List all commands by default, if someone does !help commandName then show details on that command
   if (
-    message.sender.username !== 'creativebuilds' ||
+    message.sender.username !== 'creativebuilds' &&
     message.sender.username !== config.streamer
   )
     return Promise.resolve(`You don't have permission to run that command!`);
