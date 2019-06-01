8dcf70395f4e5298df92d7ca1e9c1815f7ad8b01
Ashley Chapman <xpsx.fan@googlemail.com>
2019-06-01 20:27:52 +0100

    added help val on quote commands for people to know how to use theses commands

--- commands/quote.js
+++ commands/quote.js
@@ -7,34 +7,41 @@ const run = ({ message, args }) => {
     return new Promise((res, rej) => {
         var id = Number(args[1]);
 
-        rxQuotes.subscribe(quotes => {
+        if (args[1] !== 'help') {
+            rxQuotes.subscribe(quotes => {
 
-            var quotesArr = quotes['quotes'];
+                var quotesArr = quotes['quotes'];
 
-            if (!isNaN(id)) {
-                var quote = quotesArr[id];
-                if (id <= (quotesArr.length -1)) {
+                if (!isNaN(id)) {
+                    var quote = quotesArr[id];
+                    if (id <= (quotesArr.length -1)) {
+                        return res(
+                            `Quote ${id.toString()}: '${quote.quote}' - @${quote.quoteBy}  [${quote.date}]`     
+                        );
+                    } else {
+                        return res(`@${message.sender.dliveUsername}: The Quote you was trying to find does not exist`);
+                    }
+                } else if (args[1] === undefined || args[1] === null) {
+                    var index = quotesArr.length;
+                    var ranNumb = Number(Math.floor(Math.random() * (index - 0)));
+                    var quote = quotesArr[ranNumb];
                     return res(
-                        `Quote ${id.toString()}: '${quote.quote}' - @${quote.quoteBy}  [${quote.date}]`     
+                        `Quote ${ranNumb.toString()}: '${quote.quote}' - @${quote.quoteBy}  [${quote.date}]`     
                     );
-                } else {
-                    return res(`@${message.sender.dliveUsername}: The Quote you was trying to find does not exist`);
+                    
                 }
-            } else if (args[1] === undefined || args[1] === null) {
-                var index = quotesArr.length;
-                var ranNumb = Number(Math.floor(Math.random() * (index - 0)));
-                var quote = quotesArr[ranNumb];
-                return res(
-                    `Quote ${ranNumb.toString()}: '${quote.quote}' - @${quote.quoteBy}  [${quote.date}]`     
-                );
-                
-            }
-            else {
-                return res(
-                    `@${message.sender.dliveUsername}: You Have Entered An Invaild Value` 
-                );
-            }
-        });
+                else {
+                    return res(
+                        `@${message.sender.dliveUsername}: You Have Entered An Invaild Value` 
+                    );
+                }
+            });
+        } else {
+            return res(
+                `@${message.sender.dliveUsername}: Put a number after the command it 
+                will display selected quote if no number then it will pick a random quote` 
+            );
+        }
     });
   };
   
--- commands/removequote.js
+++ commands/removequote.js
@@ -8,28 +8,37 @@ const run = ({ message, args }) => {
     return new Promise((res, rej) => {
 
         if (message.roomRole === 'Owner' || message.roomRole === 'Moderator') {
-            rxQuotes.pipe(first()).subscribe(quotes => {
-                var id = Number(args[1]);
-                let Quotes = Object.assign({}, quotes);
+            if (args[1] !== 'help') {
+                rxQuotes.pipe(first()).subscribe(quotes => {
+                    var id = Number(args[1]);
+                    let Quotes = Object.assign({}, quotes);
 
-                if (id <= (Quotes['quotes'].length - 1)) {
-                    Quotes['quotes'].splice(id, 1);
+                    if (id <= (Quotes['quotes'].length - 1)) {
+                        Quotes['quotes'].splice(id, 1);
 
-                    for (var i = 0; i < Quotes['quotes'].length; i++) {
-                        Quotes['quotes'][i].quoteId = i;
+                        for (var i = 0; i < Quotes['quotes'].length; i++) {
+                            Quotes['quotes'][i].quoteId = i;
+                        }
+
+                        if (Quotes !== quotes) {
+                            quotes = Quotes;
+                            storage.set('quotes', Quotes);
+                        }
+
+                        return res(`@${message.sender.dliveUsername}: The Quote was successfully removed`);
+
+                    } else {
+                        return res(`@${message.sender.dliveUsername}: The Quote you was trying to find and remove does not exist`);
                     }
-
-                    if (Quotes !== quotes) {
-                        quotes = Quotes;
-                        storage.set('quotes', Quotes);
-                    }
-
-                    return res(`@${message.sender.dliveUsername}: The Quote was successfully removed`);
-
-                } else {
-                    return res(`@${message.sender.dliveUsername}: The Quote you was trying to find and remove does not exist`);
-                }
-            });
+                });
+            } else {
+                return res(
+                    `@${message.sender.dliveUsername}: Put a number after the command it 
+                    will remove selected quote` 
+                );
+            }
+        } else {
+            return res(`@${message.sender.dliveUsername}: You do not have permission to use this command`);
         }
     });
 }
