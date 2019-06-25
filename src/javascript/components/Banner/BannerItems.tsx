e76c47784fd2756e6a1b7e3cbee39613db2cbaa3
CreativeBuilds <gaminggonecreative@gmail.com>
2019-06-25 19:31:35 -0400

    Removed needless classname

--- src/javascript/components/Banner/BannerItems.tsx
+++ src/javascript/components/Banner/BannerItems.tsx
@@ -5,19 +5,31 @@ import { MdMenu, MdClose, MdEventBusy } from 'react-icons/md';
 import { BannerActionInfo } from './index';
 
 interface BannerItem {
-    message: String,
-    hasAction: Boolean,
-    actionInfo?: BannerActionInfo
+  message: String;
+  hasAction: Boolean;
+  actionInfo?: BannerActionInfo;
 }
 
-const BannerItem = ( { message, hasAction = false, actionInfo  } : BannerItem) => {
+const BannerItem = ({ message, hasAction = false, actionInfo }: BannerItem) => {
+  return (
+    <div>
+      {hasAction ? (
+        <span>
+          {`${message}: `}{' '}
+          <a
+            onClick={() => {
+              actionInfo.action();
+            }}
+            href='#'
+          >
+            {actionInfo.title}
+          </a>
+        </span>
+      ) : (
+        <span>{message}</span>
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div className={null} >
-            {hasAction ? <span>{`${message}: `} <a onClick={() => { actionInfo.action(); }} href="#">{actionInfo.title}</a></span> :
-            <span>{message}</span>
-            }
-        </div>);
-}
-
-export {BannerItem}
+export { BannerItem };
