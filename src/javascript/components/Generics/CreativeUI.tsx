b747bcefef6a520d8234dfadbab26ced3c6f64b1
Ashley Chapman <xpsx.fan@googlemail.com>
2019-07-16 12:47:55 +0100

    Update CreativeUI.tsx

--- src/javascript/components/Generics/CreativeUI.tsx
+++ src/javascript/components/Generics/CreativeUI.tsx
@@ -8,8 +8,14 @@ import {
     LinkButton
   } from "./Button";
 import { Checkbox } from './Checkbox';
-import { Page, PageHeader, PageBody } from './Common';
-import { ContextMenu, ContextItem } from './ContextMenu';
+import { Page, 
+    PageHeader, 
+    PageBody 
+    } from './Common';
+import { 
+    ContextMenu, 
+    ContextItem 
+    } from './ContextMenu';
 import { DragDrop } from './DragDrop';
 import {
     TextField,
@@ -19,38 +25,41 @@ import {
     MessageField,
     SearchField
   } from './Input';
-  import { MenuBar, MenuItem } from './MenuBar';
-  import { Panel } from './Panel';
-  import { Popup } from './Popup';
-  import { RangeSlider } from './Slider';
-  import { TitleBar } from './TitleBar';
-  import { Toggle } from './Toggle';
+import { 
+      MenuBar, 
+      MenuItem 
+    } from './MenuBar';
+import { Panel } from './Panel';
+import { Popup } from './Popup';
+import { RangeSlider } from './Slider';
+import { TitleBar } from './TitleBar';
+import { Toggle } from './Toggle';
 
-  export {
-      AdvancedDiv,
-      Button,
-      DestructiveButton,
-      ActionButton,
-      SendButton,
-      WidgetButton,
-      LinkButton,
-      Checkbox,
-      Page,
-      PageHeader,
-      PageBody,
-      ContextMenu,
-      ContextItem,
-      DragDrop,
-      TextField,
-      EmailField,
-      PasswordField,
-      StepperField,
-      MessageField,
-      SearchField,
-      MenuBar,
-      Panel,
-      Popup,
-      RangeSlider,
-      TitleBar,
-      Toggle
-  };
+export {
+    AdvancedDiv,
+    Button,
+    DestructiveButton,
+    ActionButton,
+    SendButton,
+    WidgetButton,
+    LinkButton,
+    Checkbox,
+    Page,
+    PageHeader,
+    PageBody,
+    ContextMenu,
+    ContextItem,
+    DragDrop,
+    TextField,
+    EmailField,
+    PasswordField,
+    StepperField,
+    MessageField,
+    SearchField,
+    MenuBar,
+    Panel,
+    Popup,
+    RangeSlider,
+    TitleBar,
+    Toggle
+};
