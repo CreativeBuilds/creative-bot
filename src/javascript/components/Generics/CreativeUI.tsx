6a7027612f024ac4d6f9d323145ec63a7aa4f8a6
Ashley Chapman <xpsx.fan@googlemail.com>
2019-07-16 12:16:24 +0100

    Added CreativeUI.tsx as a HeaderFile

---
+++ src/javascript/components/Generics/CreativeUI.tsx
@@ -0,0 +1,56 @@
+import { AdvancedDiv } from './AdvancedDiv';
+import {
+    Button,
+    DestructiveButton,
+    ActionButton,
+    SendButton,
+    WidgetButton,
+    LinkButton
+  } from "./Button";
+import { Checkbox } from './Checkbox';
+import { Page, PageHeader, PageBody } from './Common';
+import { ContextMenu, ContextItem } from './ContextMenu';
+import { DragDrop } from './DragDrop';
+import {
+    TextField,
+    EmailField,
+    PasswordField,
+    StepperField,
+    MessageField,
+    SearchField
+  } from './Input';
+  import { MenuBar, MenuItem } from './MenuBar';
+  import { Panel } from './Panel';
+  import { Popup } from './Popup';
+  import { RangeSlider } from './Slider';
+  import { TitleBar } from './TitleBar';
+  import { Toggle } from './Toggle';
+
+  export {
+      AdvancedDiv,
+      Button,
+      DestructiveButton,
+      ActionButton,
+      SendButton,
+      WidgetButton,
+      LinkButton,
+      Checkbox,
+      Page,
+      PageHeader,
+      PageBody,
+      ContextMenu,
+      ContextItem,
+      DragDrop,
+      TextField,
+      EmailField,
+      PasswordField,
+      StepperField,
+      MessageField,
+      SearchField,
+      MenuBar,
+      Panel,
+      Popup,
+      RangeSlider,
+      TitleBar,
+      Toggle
+  };
