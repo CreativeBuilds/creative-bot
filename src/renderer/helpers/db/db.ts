ae1c66260af9b7ff275499f274d194b310df81f3
CreativeBuilds <gaminggonecreative@gmail.com>
2019-11-09 18:47:33 -0500

    changed how uptime returned response

--- src/renderer/helpers/db/db.ts
+++ src/renderer/helpers/db/db.ts
@@ -509,7 +509,7 @@ export class Command implements ICommand {
           if(msgToSend.includes('{streamer}')){
             const firebaseUser = await rxUser.pipe(first()).toPromise();
             if(!!firebaseUser) {
-              const streamerRef = await firestore.collection('config').doc(firebaseUser.uid).get();
+              const streamerRef = await firestore.collection('configs').doc(firebaseUser.uid).get();
               const streamer = streamerRef.data();
               if(!!streamer) {
                 // tslint:disable-next-line: no-unsafe-any
@@ -519,10 +519,21 @@ export class Command implements ICommand {
           }
           if(msgToSend.includes('{uptime}')) {
             const config = await rxConfig.pipe(first()).toPromise();
+            const toHHMMSS = (secs: string) => {
+              const sec_num = parseInt(secs, 10);
+              const hours   = Math.floor(sec_num / 3600);
+              const minutes = Math.floor(sec_num / 60) % 60;
+              const seconds = sec_num % 60;
+          
+              return [hours,minutes,seconds]
+                  .map(v => v < 10 ? "0" + v : v)
+                  .filter((v,i) => v !== "00" || i > 0)
+                  .join(":");
+          }
             if(!!config) {
               // tslint:disable-next-line: no-unsafe-any
               const self = await getSelf(config.streamerAuthKey);
-              msgToSend = msgToSend.replace('{uptime}', moment(self?.livestream?.createdAt).fromNow())
+              msgToSend = msgToSend.replace('{uptime}', toHHMMSS(`${(Date.now() - Number(self?.livestream?.createdAt))/1000}`));
             }
           }
           sendMessageWithConfig(msgToSend);
